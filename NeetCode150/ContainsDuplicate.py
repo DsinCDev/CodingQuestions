a9daabb8dd653c9902bb63bcbf460d768a5e139a
DsinCDev <geoyu99@gmail.com>
2023-08-30 11:58:47 -0700

    added containsDuplicate tests

--- NeetCode150/ContainsDuplicate.py
+++ NeetCode150/ContainsDuplicate.py
@@ -1,5 +1,11 @@
 from typing import List
 
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) != len(nums)
 
-def containsDuplicate(self, nums: List[int]) -> bool:
-    return len(set(nums)) != len(nums)
+sol = Solution()
+assert sol.containsDuplicate([1,2,3,4]) == False
+assert sol.containsDuplicate([1,2,3,1]) == True
+assert sol.containsDuplicate([1,1,1,3,3,4,3,2,4,2]) == True
+assert sol.containsDuplicate([1]) == False
