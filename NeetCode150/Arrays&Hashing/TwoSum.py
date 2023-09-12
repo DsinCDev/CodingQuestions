7337c2f63619c43e771680874026956b941481fc
DsinCDev <geoyu99@gmail.com>
2023-09-11 18:40:57 -0700

    two sum

---
+++ NeetCode150/Arrays&Hashing/TwoSum.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict={}
+        for i,n in enumerate(nums):
+            if n in dict:
+                return dict[n],i
+            else:
+                dict[target-n]=i
